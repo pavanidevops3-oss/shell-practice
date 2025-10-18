0cdb3f94d6338a1ead34a054bdf701b01df4d0f3
pavani <pavanidevops3@gmail.com>
2025-10-18 02:57:34 -0400

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to Alaska Putin"
+echo "$PERSON2:: Thank you Trump, finally not on Zoom Webinar"
+echo "$PERSON1:: So, do we have a deal"
+echo "$PERSON2:: Yes, if you make Ukraine joins Russia"
