eb49b974cf14df8f06f05f04fe0e29d35b4a6765
pavani <pavanidevops3@gmail.com>
2025-10-18 09:09:49 -0400

    variables

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -2,8 +2,9 @@
 
 num=$1
 
-result-reminder=$((num % 2))
-if [ $result-reminder == 0 ] then
+res=$((num % 2))
+if [ $res == 0 ] then
     echo "Given $num is even"
 else
     echo "Given $num is odd"
+fi
